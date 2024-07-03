c29bb58ce8c7138da4aef91d8abf1065aaf7a985
Atul Tiwari <168094301+atultiwari000@users.noreply.github.com>
2024-07-03 18:13:07 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import Home from "./pages/home";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmFnh4fGeDcHa94wa2ZDNJ1wsvz7JFL1o",
-  authDomain: "soil-446c9.firebaseapp.com",
-  databaseURL: "https://soil-446c9-default-rtdb.firebaseio.com",
-  projectId: "soil-446c9",
-  storageBucket: "soil-446c9.appspot.com",
-  appId: "1:941961945328:android:e5dfc3459cfb26922ae235",
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "firebase-projectname.firebaseapp.com",
+  databaseURL: "https://projectID-default-rtdb.firebaseio.com",
+  projectId: "firebase-projectID",
+  storageBucket: "firebase-projectID.appspot.com",
+  appId: "1:XXXXXXXXXXXXX:android:XXXXXXXXXXXXXXXXXX",
 };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
